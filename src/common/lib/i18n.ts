0b54c1a76b900dd10776fc83e5865cf4387c6123
Rhseung <ryu@rhseung.me>
2026-01-02 19:39:15 +0900

    feat: add i18n

---
+++ src/common/lib/i18n.ts
@@ -0,0 +1,22 @@
+import { keyBy, mapAsync } from 'es-toolkit';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+export const SUPPORTED_LANGUAGES = ['ko', 'en'] as const;
+export type Language = (typeof SUPPORTED_LANGUAGES)[number];
+
+const localeModules = await mapAsync(SUPPORTED_LANGUAGES, async (lang) => ({
+  lang,
+  translation: (await import(`../locales/${lang}.json`)).default,
+}));
+
+i18n.use(initReactI18next).init({
+  resources: keyBy(localeModules, (item) => item.lang),
+  lng: 'ko',
+  fallbackLng: 'ko',
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
