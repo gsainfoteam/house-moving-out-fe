221d6e7b7a43dce929ff18ae792ae8983d36158e
Rhseung <ryu@rhseung.me>
2026-01-12 00:23:59 +0900

    refactor: simplify i18n resource initialization

--- src/common/lib/i18n.ts
+++ src/common/lib/i18n.ts
@@ -1,4 +1,4 @@
-import { keyBy, mapAsync } from 'es-toolkit';
+import { mapAsync } from 'es-toolkit';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
@@ -10,8 +10,12 @@ const localeModules = await mapAsync(SUPPORTED_LANGUAGES, async (lang) => ({
   translation: (await import(`../locales/${lang}.json`)).default,
 }));
 
-i18n.use(initReactI18next).init({
-  resources: keyBy(localeModules, (item) => item.lang),
+const resources = Object.fromEntries(
+  localeModules.map(({ lang, translation }) => [lang, { translation }]),
+);
+
+await i18n.use(initReactI18next).init({
+  resources,
   lng: 'ko',
   fallbackLng: 'ko',
   interpolation: {
