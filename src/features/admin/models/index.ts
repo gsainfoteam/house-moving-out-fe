eba0b0fa1bbff9f0b7b9755edae3fd8b3f7dedd9
2paperstar <lee@paperst.ar>
2026-01-28 14:06:47 +0900

    chore: remove unused

--- src/features/admin/models/index.ts
+++ src/features/admin/models/index.ts
@@ -1,5 +1,3 @@
-import type { components } from '@/@types/api-schema';
 import { ApiPaths } from '@/@types/api-schema';
 
-export type ConsentVersionInfo = components['schemas']['ConsentVersionInfo'];
 export { ApiPaths };
