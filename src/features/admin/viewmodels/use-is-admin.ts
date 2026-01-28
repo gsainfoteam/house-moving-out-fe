f4632c66b080e56f3ba1e6d73e1cf3fcfb695f85
2paperstar <lee@paperst.ar>
2026-01-28 14:06:50 +0900

    chore: remove log

--- src/features/admin/viewmodels/use-is-admin.ts
+++ src/features/admin/viewmodels/use-is-admin.ts
@@ -3,6 +3,5 @@ import { useInspectors } from './queries';
 export const useIsAdmin = () => {
   // TODO: change to user information API
   const { error } = useInspectors();
-  console.log('error', error);
   return error === undefined ? undefined : error === null;
 };
