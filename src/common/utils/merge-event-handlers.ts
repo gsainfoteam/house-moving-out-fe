bb7f38796320bdf377ae6a4d92feb34700682774
Rhseung <ryu@rhseung.me>
2026-01-29 20:32:45 +0900

    fix: correct type definition

--- src/common/utils/merge-event-handlers.ts
+++ src/common/utils/merge-event-handlers.ts
@@ -3,7 +3,7 @@ import type { SyntheticEvent } from 'react';
 export const mergeEventHandlers =
   <E extends SyntheticEvent>(
     handler1: ((event: E) => void) | undefined,
-    handler2: (event: E) => void | undefined,
+    handler2: ((event: E) => void) | undefined,
   ) =>
   (event: E) => {
     handler1?.(event);
