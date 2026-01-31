f3e3ae7970eff82588d3e815cb1a36fafd50b7a2
2paperstar <lee@paperst.ar>
2026-01-31 17:45:11 +0900

    feat: dayjs language change detection

--- src/common/utils/dayjs-init.ts
+++
@@ -1,6 +0,0 @@
-import dayjs from 'dayjs';
-import ko from 'dayjs/locale/ko';
-import localizedFormat from 'dayjs/plugin/localizedFormat';
-
-dayjs.locale(ko);
-dayjs.extend(localizedFormat);
--- src/routes/admin/index.tsx
+++ src/routes/admin/index.tsx
@@ -1,4 +1,4 @@
-import '@/common/utils/dayjs-init';
+import '@/common/lib/dayjs-init';
 import { createFileRoute } from '@tanstack/react-router';
 
 import { AdminLandingFrame } from '@/features/admin';
