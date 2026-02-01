e0f3556d6ebfee7ca1ea57dfe2cac9aec1837d90
2paperstar <lee@paperst.ar>
2026-02-01 12:21:44 +0900

    refactor: type safe loader map

--- src/common/lib/dayjs-init.ts
+++ src/common/lib/dayjs-init.ts
@@ -2,20 +2,22 @@ import dayjs from 'dayjs';
 import ko from 'dayjs/locale/ko';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
 
-import i18n from './i18n';
+import i18n, { type Language } from './i18n';
 
 dayjs.locale(ko);
 dayjs.extend(localizedFormat);
 
-i18n.on('languageChanged', async (lng) => {
+const loaderMap: Record<Language, () => Promise<ILocale>> = {
+  ko: () => import('dayjs/locale/ko'),
+  en: () => import('dayjs/locale/en'),
+};
+
+i18n.on('languageChanged', async (lng: Language) => {
   try {
-    const loader = {
-      ko: () => import('dayjs/locale/ko'),
-      en: () => import('dayjs/locale/en'),
-    }[lng];
+    const loader = loaderMap[lng];
     if (!loader) throw new Error(`Unsupported language: ${lng}`);
     const locale = await loader();
-    dayjs.locale(locale.default);
+    dayjs.locale(locale);
   } catch {
     dayjs.locale(ko);
   }
