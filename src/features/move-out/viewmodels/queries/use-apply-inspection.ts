e3035ccf155eb8903fb065fbb43aa1a21a48b01d
Rhseung <ryu@rhseung.me>
2026-02-02 20:07:36 +0900

    feat: error handling for apply inspection

--- src/features/move-out/viewmodels/queries/use-apply-inspection.ts
+++ src/features/move-out/viewmodels/queries/use-apply-inspection.ts
@@ -1,10 +1,14 @@
 import { useQueryClient } from '@tanstack/react-query';
 
+import { useTranslation } from 'react-i18next';
+import { toast } from 'sonner';
+
 import { $api } from '@/common/lib';
 
 import { ApiPaths } from '../../models';
 
 export const useApplyInspection = () => {
+  const { t } = useTranslation('move-out');
   const queryClient = useQueryClient();
 
   return $api.useMutation('post', ApiPaths.MoveOutController_applyInspection, {
@@ -12,6 +16,22 @@ export const useApplyInspection = () => {
       queryClient.invalidateQueries({
         queryKey: ['get', ApiPaths.MoveOutController_findMoveOutScheduleWithSlots],
       });
+    },
+    onError: (error) => {
+      if (error?.statusCode === 400) {
+        toast.error(t('error.badRequest', { ns: 'common' }));
+      } else if (error?.statusCode == 401) {
+        toast.error(t('error.unauthorized', { ns: 'common' }));
+      } else if (error?.statusCode == 403) {
+        toast.error(t('application.error.notStartedOrEnded'));
+      } else if (error?.statusCode == 404) {
+        toast.error(t('application.error.notFound'));
+      } else if (error?.statusCode === 409) {
+        toast.error(t('application.error.alreadyExistsOrFull'));
+        // 검사 신청이 이미 존재하거나 슬롯이 가득찬 경우 -> 다이얼로그
+      } else {
+        toast.error(t('error.internalServerError', { ns: 'common' }));
+      }
     },
   });
 };
