7ff0bb6199371e79429957525c969a6e1304ad18
Rhseung <ryu@rhseung.me>
2026-02-02 12:29:34 +0900

    feat: loading in layout card

--- src/features/move-out/views/frames/application-frame.tsx
+++ src/features/move-out/views/frames/application-frame.tsx
@@ -46,7 +46,6 @@ export function ApplicationFrame() {
   };
 
   if (!user) return null;
-  if (isLoading) return <Loading />;
 
   return (
     <div className="bg-bg-surface h-dvh px-5 py-6">
@@ -67,65 +66,73 @@ export function ApplicationFrame() {
           <img src="/house-logo.png" alt="house-logo" className="h-15" />
         </div>
 
-        {isNotFound || !isApplicationPeriod ? (
-          <LayoutCard.Root>
-            <LayoutCard.Content>
-              <LayoutCard.Header>
-                <LayoutCard.Media>
-                  <img src="./3d/not-period.png" alt="not-period" className="h-60" />
-                </LayoutCard.Media>
-                <LayoutCard.Text>
-                  <LayoutCard.Title className="text-text-black">
-                    {t('notPeriod.title')}
-                  </LayoutCard.Title>
-                </LayoutCard.Text>
-              </LayoutCard.Header>
-            </LayoutCard.Content>
-            <LayoutCard.Footer>
-              <LayoutCard.Button variant="outline" className="w-full">
-                {t('notPeriod.button')}
-              </LayoutCard.Button>
-            </LayoutCard.Footer>
-          </LayoutCard.Root>
-        ) : (
-          <LayoutCard.Root>
-            <LayoutCard.Content>
-              <LayoutCard.Header>
-                <LayoutCard.Text>
-                  <LayoutCard.Title className="text-primary-main">
-                    {t('application.title')}
-                  </LayoutCard.Title>
-                  <LayoutCard.Description>{t('application.description')}</LayoutCard.Description>
-                </LayoutCard.Text>
-              </LayoutCard.Header>
-              <div className="h-full w-full">
-                <DateSelect days={inspectionDays} value={selectedDay} onChange={handleDayChange} />
-                {selectedDay && (
-                  <div className="mt-6 grid grid-cols-3 gap-2">
-                    {selectedDaySlots.map((slot) => (
-                      <TimeSelect
-                        key={slot.uuid}
-                        slot={slot}
-                        value={selectedSlotUuid}
-                        onChange={(slot) =>
-                          setSelectedSlotUuid((prev) => (prev === slot.uuid ? null : slot.uuid))
-                        }
-                      />
-                    ))}
-                  </div>
-                )}
-              </div>
-            </LayoutCard.Content>
-            <LayoutCard.Footer className="mt-auto">
-              <LayoutCard.Button variant="outline">
-                {t('application.button.cancel')}
-              </LayoutCard.Button>
-              <LayoutCard.Button variant="default" className="w-full">
-                {t('application.button.next')}
-              </LayoutCard.Button>
-            </LayoutCard.Footer>
-          </LayoutCard.Root>
-        )}
+        <LayoutCard.Root>
+          {isLoading ? (
+            <Loading className="h-full" />
+          ) : isNotFound || !isApplicationPeriod ? (
+            <>
+              <LayoutCard.Content>
+                <LayoutCard.Header>
+                  <LayoutCard.Media>
+                    <img src="./3d/not-period.png" alt="not-period" className="h-60" />
+                  </LayoutCard.Media>
+                  <LayoutCard.Text>
+                    <LayoutCard.Title className="text-text-black">
+                      {t('notPeriod.title')}
+                    </LayoutCard.Title>
+                  </LayoutCard.Text>
+                </LayoutCard.Header>
+              </LayoutCard.Content>
+              <LayoutCard.Footer>
+                <LayoutCard.Button variant="outline" className="w-full">
+                  {t('notPeriod.button')}
+                </LayoutCard.Button>
+              </LayoutCard.Footer>
+            </>
+          ) : (
+            <>
+              <LayoutCard.Content>
+                <LayoutCard.Header>
+                  <LayoutCard.Text>
+                    <LayoutCard.Title className="text-primary-main">
+                      {t('application.title')}
+                    </LayoutCard.Title>
+                    <LayoutCard.Description>{t('application.description')}</LayoutCard.Description>
+                  </LayoutCard.Text>
+                </LayoutCard.Header>
+                <div className="h-full w-full">
+                  <DateSelect
+                    days={inspectionDays}
+                    value={selectedDay}
+                    onChange={handleDayChange}
+                  />
+                  {selectedDay && (
+                    <div className="mt-6 grid grid-cols-3 gap-2">
+                      {selectedDaySlots.map((slot) => (
+                        <TimeSelect
+                          key={slot.uuid}
+                          slot={slot}
+                          value={selectedSlotUuid}
+                          onChange={(slot) =>
+                            setSelectedSlotUuid((prev) => (prev === slot.uuid ? null : slot.uuid))
+                          }
+                        />
+                      ))}
+                    </div>
+                  )}
+                </div>
+              </LayoutCard.Content>
+              <LayoutCard.Footer className="mt-auto">
+                <LayoutCard.Button variant="outline">
+                  {t('application.button.cancel')}
+                </LayoutCard.Button>
+                <LayoutCard.Button variant="default" className="w-full">
+                  {t('application.button.next')}
+                </LayoutCard.Button>
+              </LayoutCard.Footer>
+            </>
+          )}
+        </LayoutCard.Root>
       </div>
     </div>
   );
