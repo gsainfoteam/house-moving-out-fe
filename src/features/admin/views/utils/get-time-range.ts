13ac77f30d5ebbb7489f99cb6d6a1759054ba787
2paperstar <lee@paperst.ar>
2026-02-07 22:28:01 +0900

    refactor: use range

--- src/features/admin/views/utils/get-time-range.ts
+++ src/features/admin/views/utils/get-time-range.ts
@@ -1,10 +1,12 @@
+import { range } from 'es-toolkit';
+
 import type dayjs from 'dayjs';
 
 type Range = { start: dayjs.Dayjs; end: dayjs.Dayjs };
 export const getTimeRange = (list: Range[]): Range[] => {
   if (list.length === 0) return [];
 
-  const unseen = new Set([...Array(list.length)].map((_, i) => i));
+  const unseen = new Set(range(list.length));
   const queue = [0];
 
   const result: Range[] = [];
