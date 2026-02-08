cca63f06ffda9e6f3b73a5ec24e43ee0b118e05c
2paperstar <lee@paperst.ar>
2026-02-09 01:05:05 +0900

    fix: loading style

--- src/features/admin/views/frames/inspectors-list-frame.tsx
+++ src/features/admin/views/frames/inspectors-list-frame.tsx
@@ -24,7 +24,7 @@ export function InspectorsListFrame() {
 
   if (isScheduleNotFound || isInspectorsNotFound)
     return <div className="p-4">{t('schedule.detail.notFound')}</div>;
-  if (!schedule || !inspectors) return <Loading className="flex-1" />;
+  if (!schedule || !inspectors) return <Loading containerClassName="h-auto flex-1" />;
 
   const maleSlotTimes = inspectors
     .filter((i) => i.gender === Gender.MALE)
