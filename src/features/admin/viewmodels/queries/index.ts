b27f2c2800e82e8a08e88f888fece717927412b6
2paperstar <lee@paperst.ar>
2026-02-09 00:45:04 +0900

    feat: merge create schedule, target api

--- src/features/admin/viewmodels/queries/index.ts
+++ src/features/admin/viewmodels/queries/index.ts
@@ -1,4 +1,3 @@
-export * from './use-compare-sheets';
 export * from './use-create-move-out-schedule';
 export * from './use-find-all-move-out-schedules';
 export * from './use-get-move-out-schedule';
--- src/features/admin/viewmodels/queries/use-create-move-out-schedule.ts
+++ src/features/admin/viewmodels/queries/use-create-move-out-schedule.ts
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next';
 import { toast } from 'sonner';
 
 import { $api } from '@/common/lib';
+import { multipartSerializer } from '@/common/utils';
 
 import { ApiPaths } from '../../models';
 
@@ -11,7 +12,10 @@ export const useCreateMoveOutSchedule = () => {
   const queryClient = useQueryClient();
   const { t } = useTranslation('admin');
 
-  return $api.useMutation('post', ApiPaths.MoveOutController_createMoveOutSchedule, {
+  return $api.useMutation('post', ApiPaths.MoveOutController_createMoveOutScheduleWithTargets, {
+    onMutate(variables) {
+      variables.bodySerializer = multipartSerializer;
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ['get', ApiPaths.MoveOutController_findAllMoveOutSchedules],
