e7d72aa10f237a059ff4ebf8e7f00c8f930ab484
Rhseung <ryu@rhseung.me>
2026-02-14 00:50:22 +0900

    feat: add useSubmitInspectionResult

--- src/features/inspector/models/index.ts
+++ src/features/inspector/models/index.ts
@@ -1,1 +1,4 @@
+import type { components } from '@/@types/api-schema';
+
+export type SubmitInspectionResultForm = components['schemas']['SubmitInspectionResultFormDto'];
 export { ApiPaths, MoveOutScheduleResDtoStatus as ScheduleStatus } from '@/@types/api-schema';
