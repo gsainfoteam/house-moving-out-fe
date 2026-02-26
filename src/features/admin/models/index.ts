faf340d1145ecaea4f7295642caaf29a9f27b8bb
2paperstar <lee@paperst.ar>
2026-02-26 14:52:17 +0900

    feat: room visualize

--- src/features/admin/models/index.ts
+++ src/features/admin/models/index.ts
@@ -2,6 +2,7 @@ import { type components } from '@/@types/api-schema';
 
 export type MoveOutSchedule = components['schemas']['MoveOutScheduleResDto'];
 export type InspectionSlot = components['schemas']['InspectionSlotResDto'];
+export type Target = components['schemas']['InspectionTargetsGroupedByRoomResDto'];
 export {
   PathsMoveOutInspectionTargetsGetParametersQueryCurrentSeason as Season,
   InspectorResDtoGender as Gender,
