6b2096b397f4e2009ef682b6edabf8741c8eebb8
2paperstar <lee@paperst.ar>
2026-02-26 15:00:29 +0900

    fix: use self contained model, viewmodel

--- src/features/admin/views/components/room-visualize/index.tsx
+++ src/features/admin/views/components/room-visualize/index.tsx
@@ -7,8 +7,9 @@ import { useTranslation } from 'react-i18next';
 
 import { Loading } from '@/common/components';
 import { cn } from '@/common/utils';
-import { InspectionType, type Target } from '@/features/admin/models';
-import { useTargets } from '@/features/admin/viewmodels';
+
+import { InspectionType, type Target } from '../../../models';
+import { useTargets } from '../../../viewmodels';
 
 const config = {
   G: [19, 19, 19, 19, 19, 19],
