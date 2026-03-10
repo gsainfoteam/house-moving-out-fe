00b7438811cded7ed0b6050a4dbe61211340851f
2paperstar <lee@paperst.ar>
2026-03-10 13:15:47 +0900

    feat: a11y for sample image button

--- src/features/inspector/views/components/sample-image-button.tsx
+++ src/features/inspector/views/components/sample-image-button.tsx
@@ -6,10 +6,10 @@ function SampleImageOverlay({ close, image }: { close: () => void; image: string
   return (
     <div className="fixed inset-0 p-4">
       <div className="bg-bg-white relative top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col gap-4 rounded-2xl p-4 shadow-2xl">
-        <button onClick={close} className="cursor-pointer self-end">
+        <button onClick={close} className="cursor-pointer self-end" aria-label="close">
           <XCloseIcon />
         </button>
-        <img src={image} className="rounded-2xl" />
+        <img src={image} className="rounded-2xl" alt="sample image" />
       </div>
     </div>
   );
