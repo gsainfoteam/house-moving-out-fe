1befe909517cfb6a5c3d8108b0c31c5e4857f5e0
Rhseung <ryu@rhseung.me>
2026-03-15 22:22:36 +0900

    feat: add segment control in article list

--- src/features/user/views/components/index.ts
+++ src/features/user/views/components/index.ts
@@ -2,3 +2,4 @@ export * from './date-select';
 export * from './steps';
 export * from './time-select';
 export * from './article-card';
+export * from './article-type-segment';
--- src/features/user/views/frames/article-list-frame.tsx
+++ src/features/user/views/frames/article-list-frame.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { Link } from '@tanstack/react-router';
 
 import { useTranslation } from 'react-i18next';
@@ -6,32 +8,35 @@ import { LayoutCard } from '@/common/components';
 
 import { ArticleType } from '../../models';
 import { useFindArticles } from '../../viewmodels';
-import { ArticleCard } from '../components';
+import { ArticleCard, ArticleTypeSegment } from '../components';
 
 export function ArticleListFrame() {
   const { t } = useTranslation('user');
-  // TODO: 타입 선택 기능 추가 - layout card 위에 segment control 추가
-  const { articles, isLoading } = useFindArticles({ type: ArticleType.NOTICE });
+  const [type, setType] = useState<ArticleType>(ArticleType.NOTICE);
+  const { articles, isLoading } = useFindArticles({ type });
 
   return (
-    <LayoutCard.Root isLoading={isLoading}>
-      <LayoutCard.Header>
-        <LayoutCard.Text>
-          <LayoutCard.Title>{t('list.title')}</LayoutCard.Title>
-        </LayoutCard.Text>
-      </LayoutCard.Header>
-      <LayoutCard.Body className="gap-3">
-        {articles.map((article) => (
-          <Link
-            key={article.uuid}
-            to="/articles/$uuid"
-            params={{ uuid: article.uuid }}
-            className="w-full"
-          >
-            <ArticleCard article={article} />
-          </Link>
-        ))}
-      </LayoutCard.Body>
-    </LayoutCard.Root>
+    <div className="flex h-full flex-col gap-4">
+      <ArticleTypeSegment value={type} onChange={setType} />
+      <LayoutCard.Root isLoading={isLoading}>
+        <LayoutCard.Header className="gap-4">
+          <LayoutCard.Text>
+            <LayoutCard.Title>{t('list.title')}</LayoutCard.Title>
+          </LayoutCard.Text>
+        </LayoutCard.Header>
+        <LayoutCard.Body className="gap-3">
+          {articles.map((article) => (
+            <Link
+              key={article.uuid}
+              to="/articles/$uuid"
+              params={{ uuid: article.uuid }}
+              className="w-full"
+            >
+              <ArticleCard article={article} />
+            </Link>
+          ))}
+        </LayoutCard.Body>
+      </LayoutCard.Root>
+    </div>
   );
 }
