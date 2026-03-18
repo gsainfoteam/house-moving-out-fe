2bcefff041cbfe1ad89f98dc5694ab7b7eb46855
2paperstar <lee@paperst.ar>
2026-03-18 20:51:28 +0900

    feat: color by percentage

--- src/features/admin/views/frames/admin-layout-frame.tsx
+++ src/features/admin/views/frames/admin-layout-frame.tsx
@@ -22,6 +22,29 @@ function SmallScreenBlocker() {
   );
 }
 
+function DatabaseSizeBar() {
+  const { t } = useTranslation('admin');
+  const { data } = useDatabaseSize();
+  if (!data) return null;
+
+  const percentage = (data.bytes / (1024 * 1024 * 500)) * 11;
+  return (
+    <div className="flex items-center gap-2">
+      {`${t('databaseSize')} (${data.pretty}/500MB)`}
+      <div className="relative h-3 w-36 overflow-hidden rounded-2xl bg-gray-200">
+        <div
+          className={cn(
+            'bg-primary-main absolute left-0 h-3',
+            percentage >= 80 && 'bg-yellow-500',
+            percentage >= 90 && 'bg-red-500',
+          )}
+          style={{ width: `${percentage}%` }}
+        />
+      </div>
+    </div>
+  );
+}
+
 export function AdminLayoutFrame() {
   const { t } = useTranslation('admin');
   const { data } = useDatabaseSize();
@@ -58,17 +81,7 @@ export function AdminLayoutFrame() {
               {t('article.list.nav')}
             </Link>
           </nav>
-          {data && (
-            <div className="flex items-center gap-2">
-              {`${t('databaseSize')} (${data.pretty}/500MB)`}
-              <div className="relative h-3 w-36 overflow-hidden rounded-2xl bg-gray-200">
-                <div
-                  className="bg-primary-main absolute left-0 h-3"
-                  style={{ width: `${(data.bytes / (1024 * 1024 * 500)) * 100}%` }}
-                />
-              </div>
-            </div>
-          )}
+          <DatabaseSizeBar />
           <div className="px-2">
             <LanguageToggle />
           </div>
