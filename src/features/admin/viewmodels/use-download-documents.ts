c70e1ad30c8aec3c53ec3f59fadac4744fb2cb6c
2paperstar <lee@paperst.ar>
2026-03-22 14:10:48 +0900

    feat: error handling for download documents

--- src/features/admin/viewmodels/use-download-documents.ts
+++ src/features/admin/viewmodels/use-download-documents.ts
@@ -1,5 +1,8 @@
 import { useCallback, useTransition } from 'react';
 
+import { useTranslation } from 'react-i18next';
+import { toast } from 'sonner';
+
 import { api } from '@/common/lib';
 
 import { ApiPaths } from '../models';
@@ -8,26 +11,31 @@ import { useGetMoveOutScheduleQuery } from './queries';
 export const useDownloadDocuments = (scheduleUuid: string) => {
   const [isDownloading, startTransition] = useTransition();
   const { data } = useGetMoveOutScheduleQuery(scheduleUuid);
+  const { t } = useTranslation('admin');
 
   const download = useCallback(
     () =>
       startTransition(async () => {
         if (!data) return;
-        const response = await api.GET(ApiPaths.ScheduleController_downloadInspectionDocuments, {
-          params: { path: { uuid: scheduleUuid } },
-          parseAs: 'blob',
-        });
-        if (!response.data) return;
-        const url = URL.createObjectURL(response.data);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = `${data.title}.pdf`;
-        document.body.appendChild(a);
-        a.click();
-        window.URL.revokeObjectURL(url);
-        document.body.removeChild(a);
+        try {
+          const response = await api.GET(ApiPaths.ScheduleController_downloadInspectionDocuments, {
+            params: { path: { uuid: scheduleUuid } },
+            parseAs: 'blob',
+          });
+          if (!response.data) return;
+          const url = URL.createObjectURL(response.data);
+          const a = document.createElement('a');
+          a.href = url;
+          a.download = `${data.title}.pdf`;
+          document.body.appendChild(a);
+          a.click();
+          window.URL.revokeObjectURL(url);
+          document.body.removeChild(a);
+        } catch {
+          toast.error(t('error.internalServerError', { ns: 'common' }));
+        }
       }),
-    [data, scheduleUuid],
+    [data, scheduleUuid, t],
   );
 
   return { download, isDownloading };
