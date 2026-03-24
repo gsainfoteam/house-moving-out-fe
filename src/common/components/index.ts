a32669bd501af0f2959170448ac739afbd58c072
Rhseung <ryu@rhseung.me>
2026-03-24 15:35:27 +0900

    refactor: remove unused Fab component

--- src/common/components/index.ts
+++ src/common/components/index.ts
@@ -1,7 +1,6 @@
 export * from './accordion';
 export * from './dialog';
 export * from './drawer';
-export * from './fab';
 export * from './language-toggle';
 export * from './layout';
 export * from './layout-card';
