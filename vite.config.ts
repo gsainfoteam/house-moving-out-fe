7bf1fa216593b09f225e94642aed628a17dfc709
2paperstar <lee@paperst.ar>
2026-04-01 01:08:36 +0900

    fix: wrong title

--- vite.config.ts
+++ vite.config.ts
@@ -28,9 +28,9 @@ export default defineConfig(({ mode }) => {
         registerType: 'autoUpdate',
         includeAssets: ['favicon.png', 'favicon.ico', 'pwa/*.png'],
         manifest: {
-          name: '하우스 퇴사검사',
+          name: '하우스연합회 퇴사검사',
           short_name: '퇴사검사',
-          description: '하우스 퇴사검사',
+          description: '하우스연합회 퇴사검사',
           theme_color: '#ffffff',
           background_color: '#ffffff',
           display: 'standalone',
