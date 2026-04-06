852e8cc62243a7fa9eb38c880c53d777a257df23
Boseong <lee@paperst.ar>
2026-04-06 20:27:49 +0900

    feat: sort, filter in inspection list (#190)

--- src/features/inspector/views/screens/inspection-list-screen.tsx
+++ src/features/inspector/views/screens/inspection-list-screen.tsx
@@ -1,15 +1,87 @@
 import { Link } from '@tanstack/react-router';
 
 import dayjs from 'dayjs';
-import { Calendar } from 'lucide-react';
+import { Calendar, ListFilterIcon } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
 
-import { LayoutCard, useList } from '@/common/components';
+import { Button, Drawer, LayoutCard, useList } from '@/common/components';
+import { overlay } from '@/common/lib';
+import { cn } from '@/common/utils';
 
 import { type ApplicationStatus } from '../../viewmodels';
 import { InspectionScheduleCard } from '../components';
 
-export function InspectionListScreen({ targets, isLoading }: InspectionListScreen.Props) {
+// t('filter.items.all')
+// t('filter.items.today')
+// t('filter.items.not_completed')
+const filters = ['all', 'today', 'not_completed'] as const;
+type Filter = (typeof filters)[number];
+
+const FilterDrawer = ({
+  currentFilter,
+  onFilterChange,
+}: {
+  currentFilter: Filter;
+  onFilterChange: (filter: Filter) => void;
+}) => {
+  const { t } = useTranslation('inspector');
+  return (
+    <>
+      <Drawer.Header>
+        <Drawer.Title>{t('filter.title')}</Drawer.Title>
+      </Drawer.Header>
+      <Drawer.Body>
+        {filters.map((filter) => (
+          <Drawer.Close asChild key={filter}>
+            <button
+              className={cn(
+                'text-body-lg text-text-primary flex w-full items-center gap-3 rounded-xl px-4 py-3',
+                filter === currentFilter && 'bg-primary text-text-white',
+              )}
+              onClick={() => onFilterChange(filter)}
+            >
+              {t(`filter.items.${filter}`)}
+            </button>
+          </Drawer.Close>
+        ))}
+      </Drawer.Body>
+    </>
+  );
+};
+
+function FilterButton({
+  filter,
+  onFilterChange,
+}: {
+  filter: Filter;
+  onFilterChange: (filter: Filter) => void;
+}) {
+  const { t } = useTranslation('inspector');
+  return (
+    <Button
+      aria-label={t('filter.title')}
+      className={cn('self-end', filter !== 'all' && 'bg-primary')}
+      variant="subtle"
+      size="icon"
+      onClick={() =>
+        overlay.open(() => (
+          <Drawer.Root>
+            <FilterDrawer currentFilter={filter} onFilterChange={onFilterChange} />
+          </Drawer.Root>
+        ))
+      }
+    >
+      <ListFilterIcon className={cn('size-4', filter !== 'all' && 'text-text-white')} />
+    </Button>
+  );
+}
+
+export function InspectionListScreen({
+  targets,
+  isLoading,
+  filter,
+  onFilterChange,
+}: InspectionListScreen.Props) {
   const { t } = useTranslation('inspector');
   const list = useList(targets);
 
@@ -21,14 +93,15 @@ export function InspectionListScreen({ targets, isLoading }: InspectionListScree
           <LayoutCard.Description>{t('list.description')}</LayoutCard.Description>
         </LayoutCard.Text>
       </LayoutCard.Header>
-      <LayoutCard.Body>
+      <LayoutCard.Body className="gap-4">
+        <FilterButton filter={filter} onFilterChange={onFilterChange} />
         <list.Root className="gap-3">
           <list.Empty
             icon={<Calendar />}
             title={t('inspector.emptySchedule')}
             description={t('inspector.emptyScheduleDescription')}
           />
-          <list.Builder className="flex w-full flex-col gap-3">
+          <list.Builder className="flex w-full flex-col gap-2">
             {(target) => (
               <Link
                 to="/inspector/$uuid"
@@ -63,5 +136,7 @@ export namespace InspectionListScreen {
   export type Props = {
     targets: Target[];
     isLoading: boolean;
+    filter: Filter;
+    onFilterChange: (filter: Filter) => void;
   };
 }
